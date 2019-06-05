4c475bfdc853d9ac34adfc82957e778b7214aa53
PetrPetr <ferel2015@yandex.ru>
2019-06-05 18:39:25 +0300

    lesson cars

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarApp
+{
+   public class Car
+    {
+        public int id { get; set; }
+        public string Marka { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+        public int Year { get; set; }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace CarApp
+{
+    class Program
+    {
+        public class ApplicationContext:DbContext
+        {
+            public DbSet<Car> Cars { get; set; }
+
+            public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDb;database = CarApp;Trusted_Connection = True;");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Car car1 = new Car { Marka = "Nissan", Color = "Yellow", Price = 1000000, Year = 2018};
+                Car car2 = new Car { Marka = "Volvo", Color = "Brown", Price = 80000, Year = 2000};
+
+                db.Cars.Add(car1);
+                db.Cars.Add(car2);
+                db.SaveChanges();
+                Console.WriteLine("Done");
+
+                var cars = db.Cars.ToList();
+                foreach (Car c in cars)
+                    Console.WriteLine("{0} {1} {2} ({3}) {4}", c.Marka, c.Price, c.Color, c.Year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
